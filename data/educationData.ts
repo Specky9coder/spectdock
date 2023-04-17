96fe7e249b0ea6d261a05b35967073b3db148d80
Specky9coder <48853217+Specky9coder@users.noreply.github.com>
2023-04-17 17:45:06 +0530

    Add files via upload

---
+++ data/educationData.ts
@@ -0,0 +1,16 @@
+const educationData = [
+  {
+    title: 'Diploma Information Technology',
+    company: 'GTU',
+    location: 'Ahemdabad, India',
+    range: 'Aug 2016 - Jun 2019',
+    url: 'https://www.gtu.ac.in/',
+    text1: 'Graduated with CGPA of 7.2',
+    text2:
+      'Spent 3 years learning and mastering Software Engineering, Machine Learning, Statistics and Programming.',
+    text3:
+      'Developed Systems to generate music analyzing the pain through the brain singnal with signal processing and Natural Laguage processing (NLP).',
+  },
+]
+
+export default educationData
